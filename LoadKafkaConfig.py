128282fb44405302b33df4fb14ade7cf6fd775cf
Itajiba <itajiba@gmail.com>
2025-05-28 13:48:33 -0300

    Mais exemplos

---
+++ LoadKafkaConfig.py
@@ -0,0 +1,10 @@
+def load_config(file_path):
+    config = {}
+    with open(file_path) as f:
+        for line in f:
+            if line.strip() and not line.startswith('#'):
+                key, value = line.strip().split('=', 1)
+                config[key.strip()] = value.strip()
+    return config
+
+config = load_config('producer.properties')
